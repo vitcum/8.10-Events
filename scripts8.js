96bd8a47a0222c565f836acf7af32b8660a56425
vitcum <vitcum@gmail.com>
2016-11-28 20:58:34 +0100

    Add files via upload

--- scripts8.js
+++ scripts8.js
@@ -2,5 +2,5 @@ var list = document.getElementById('Lista')
 	add = document.getElementById('addElem');
 
 add.addEventListener('click', function(e) {
-	list.innerHTML += '<li>item</li>';
+	list.innerHTML += '<li>item ' + list.childElementCount + '</li>';
 });
